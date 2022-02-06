5409206500d1372d382cb1d9423d49499758b374
MangelH <migeuhoyos22@gmail.com>
2022-02-06 01:40:40 -0500

    add new folder of course

---
+++ 5-booleanos/metodosBooleanos.py
@@ -0,0 +1,8 @@
+cadena = "Estoy mostrando los metodos booleanos para las string"
+cadena2 = "CADCKANVOADVJOAD"
+
+print(cadena.startswith("e"))
+print(cadena2.startswith("C"))
+
+print(cadena2.isupper()) #si toda la cadena esta en mayusculas
+print(cadena.islower()) #si la cadena esta toda en minuscula
---
+++ 5-booleanos/operadoresLogicos.py
@@ -0,0 +1,5 @@
+print(10 > 2 and 10 == 2)
+
+print(99 != 99 or 99 > 80)
+
+print(not 90 != 90) #not dobla el resultado si es true es false pero si es false es true
